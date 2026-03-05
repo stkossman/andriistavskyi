a0bc60fa3ffbd5228268657a1a705364a4ad9c5a
Andrii Stavskyi <an.stawski@outlook.com>
2026-03-05 16:54:29 +0200

    feat: enhance Clock component styling and add AnimatedLink component for footer

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -26,7 +26,7 @@ export function Clock() {
 
 	return (
 		<div className='px-4 py-2 border-2 rounded-full border-text-primary'>
-			<div className='text-sm font-heading text-text-primary uppercase tracking-wider'>
+			<div className='text-sm text-text-primary uppercase tracking-wider'>
 				{time}
 			</div>
 		</div>
