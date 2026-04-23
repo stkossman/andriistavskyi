37da91affce77dc07f6d65775b51d0f0c70a25fa
Andrii Stavskyi <an.stawski@outlook.com>
2026-04-23 21:55:41 +0300

    feat: fix strict typescript types

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module '@fontsource-variable/unbounded'
+declare module '@fontsource/arimo'
+declare module '@fontsource-variable/arimo'
