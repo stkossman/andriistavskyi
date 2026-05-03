1e697e8281dcc221963a76c8c627ac704d3b7c93
Andrii Stavskyi <an.stawski@outlook.com>
2026-05-03 14:48:53 +0300

    test: simplify error page message content to test gemini and ci

--- src/const/error.ts
+++ src/const/error.ts
@@ -1,9 +1,5 @@
 export const ERROR_TEXT = {
 	TITLE: '404',
-	MESSAGE_LINES: [
-		'Page not found.',
-		'Ratio: 0/1.',
-		'This is an acceptable loss.',
-	],
+	MESSAGE_LINES: ['Page not found.', 'Ratio: 0/1.'],
 	BUTTON: 'Resume operations',
 } as const
