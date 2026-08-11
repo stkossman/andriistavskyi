2e9dd98dddc170727d53169f8f0179ad5c8c896c
Kossman <an.stawski@outlook.com>
2026-08-12 02:59:45 +0300

    style: add restrained retro color accents

--- src/const/education.ts
+++ src/const/education.ts
@@ -2,6 +2,6 @@ export const EDUCATION = [
 	{
 		institution: 'National University of Ostroh Academy',
 		program: 'Computer Science',
-		period: '2023 - Present',
+		period: '2023 - 2027',
 	},
 ] as const
