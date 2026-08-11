85b84dff3b78b2215ffe9ad0675c66d6763d12e5
Kossman <an.stawski@outlook.com>
2026-08-12 02:41:49 +0300

    refactor: replace tailwind with colocated scss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 
 import sitemap from '@astrojs/sitemap'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.andriistavskyi.me/',
 	integrations: [sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
 })
