d1aeb1d215ff9c94507dcfe72a7fc53a15e1eb5d
Kossman <an.stawski@outlook.com>
2026-08-12 14:08:50 +0300

    feat: restructure about page and home page components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
+import { fileURLToPath } from 'node:url'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.andriistavskyi.me/',
 	integrations: [sitemap()],
+	vite: {
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+	},
 })
